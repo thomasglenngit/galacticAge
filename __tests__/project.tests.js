45d167b500854de45d10db31360209ec8798d5c8
Thomas Glenn <thomasgrantglenn@mac.com>
2020-06-26 14:29:15 -0600

    record planetAge method (adding 'earth' string) test4 -passed.

--- __tests__/project.tests.js
+++ __tests__/project.tests.js
@@ -9,6 +9,6 @@ describe('Human class', () => {
   test('should calculate human age in days, and then divide by days', () => {
     let testEarthAge = new Human(43, 80, "earth");
     testEarthAge.planetAge();
-    expect(testEarthAge.age).toEqual(15695, 37);
+    expect(testEarthAge.age).toEqual(15695, 37, "earth");
   });
 });
